04e938df66be7812a685dfbe775895b0e4f35e71
DamianoMura <mura.damiano14@gmail.com>
2025-11-30 19:53:24 +0100

    [fix]string lenght error

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -20,7 +21,13 @@
         @endif
     </head>
     <body>
+      <header>
+        <!--Header-->
+        @include('partials.header')
+        
+      </header>
         <div class="container">
+            
             @yield('content')
         </div>
     </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="top-header">
+        <div class="container d-flex justify-content-end">
+            <span class="me-3">DC POWER℠ VISA®</span>
+            <span>ADDITIONAL DC SITES ▼</span>
+        </div>
+    </div>
+</header>
