ecb2820dcf1d4c9a8143cf78ce9caa9dcfdd68e6
DamianoMura <mura.damiano14@gmail.com>
2025-12-03 10:44:47 +0100

    [upd] set skeleton layout to improve the addition of different layouts without having to rewrite headers and scripts tags everytime

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,7 +7,7 @@
         <h2 class="comics-label">
             current series
         </h2>
-
+    
     <div class="art-3 row-start-3 grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         @foreach ($comics as $comic)
         <div class="comic-card my-3">
@@ -17,7 +17,11 @@
         </div>
         @endforeach
     </div>
-    
+</div>
+<div class="text-center mt-2">
+    <button class="btn uppercase bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  mt-6">
+        load more
+    </button>
 </div>
         </main>
 @endsection
